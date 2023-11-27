251ec875f20045a5279a05d1b002068cb62dabd4
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-27 16:20:56 -0300

    fix: Push missing files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,38 @@
-<main class="bg-slate-500 text-black flex justify-center items-center h-screen">
-  <h1 class="text-3xl bg-slate-200 rounded p-6 m-5 text-center">
-    <span class="pr-3 text-4xl">🎵</span>Hello MusikMorpher!
-  </h1>
-</main>
+<script>
+  let files;
 
+  async function uploadFile() {
+    const formData = new FormData();
+    formData.append("file", files[0]);
+
+    try {
+      const response = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const fileUrl = await response.text();
+        window.location.href = fileUrl;
+      } else {
+        console.error("File upload failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  }
+</script>
+
+<main class="bg-slate-500 text-black flex justify-center items-center h-screen">
+  <form
+    id="uploadForm"
+    class="text-center"
+    method="post"
+    on:submit|preventDefault={uploadFile}
+  >
+    <input type="file" id="mp3File" accept=".mp3" bind:files={files} />
+    <button type="submit" class="bg-white uppercase rounded p-3 mt-4">
+      Upload
+    </button>
+  </form>
+</main>
